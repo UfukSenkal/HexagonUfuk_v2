12e55ee5f3fa75c8797b8dc6adf3f42bfd7562fa
UfukSenkal <67637340+UfukSenkal@users.noreply.github.com>
2021-10-13 04:15:19 +0300

    +created map state for tests

---
+++ Assets/Scripts/Map/MapState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum GameState
+{
+    Moving,
+    Filled,
+    Rotating,
+    Explode
+}
+public static class MapState
+{
+    public static GameState GameStateInfo;
+}
