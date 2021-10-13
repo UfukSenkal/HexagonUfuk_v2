d2617fa249af28a131f7ac02eda9d3ebccb16ae7
UfukSenkal <67637340+UfukSenkal@users.noreply.github.com>
2021-10-13 04:15:02 +0300

    +created input system for tests

---
+++ Assets/Scripts/Input/InputData.cs
@@ -0,0 +1,93 @@
+﻿using HexagonDemo.Hexagon;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HexagonDemo.InputData
+{
+    [CreateAssetMenu(menuName = "HexagonDemo/Input/InputData")]
+    public class InputData : ScriptableObject
+    {
+        private List<IHexagon> _lastSelectionList = new List<IHexagon>();
+        [SerializeField] private GameObject centerObjPrefab;
+        private GameObject _instantiatedCenterObj;
+
+        public void Click()
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (hit && hit.transform.TryGetComponent<HexagonController>(out HexagonController neighbour))
+            {
+                ClearLastSelection();
+                SelectNeighbours(mousePos, neighbour);
+
+                
+            }
+        }
+
+        private void SelectNeighbours(Vector2 mousePos, HexagonController neighbour)
+        {
+            neighbour.InstantiatedNeighbourData.GetClosestNeighbours(mousePos);
+            for (int i = 0; i < neighbour.InstantiatedNeighbourData.GetClosestNeighbours(mousePos).Count; i++)
+            {
+                neighbour.InstantiatedNeighbourData.GetClosestNeighbours(mousePos)[i].Outline.SetActive(true);
+                _lastSelectionList.Add(neighbour.InstantiatedNeighbourData.GetClosestNeighbours(mousePos)[i]);
+                
+
+            }
+            FindCenter();
+            
+        }
+
+        private void ClearLastSelection()
+        {
+
+            
+            for (int i = 0; i < _lastSelectionList.Count; i++)
+            {
+                if (_lastSelectionList[i].Outline != null)
+                {
+
+                    _lastSelectionList[i].Outline.SetActive(false);
+                    _lastSelectionList[i].SelfGameObject.transform.SetParent(null);
+                }
+            }
+            Destroy(_instantiatedCenterObj);
+            _lastSelectionList.Clear();
+        }
+        public void RotateHexagons()
+        {
+            //_instantiatedCenterObj.transform.Rotate(0, 0, 120f, Space.Self);
+            //Debug.Log(_instantiatedCenterObj.transform.eulerAngles);
+            _lastSelectionList[0].SelfGameObject.GetComponent<HexagonRotationController>().RotateHexagons(_lastSelectionList,_instantiatedCenterObj);
+        }
+
+        private Vector2 FindCenter()
+        {
+            Vector3 center = Vector2.zero;
+
+            foreach (var neighbour in _lastSelectionList)
+            {
+                center += neighbour.SelfGameObject.transform.position;
+            }
+            center /= 3;
+            InstantiateCenterObj(center);
+            return center;
+        }
+
+        private void InstantiateCenterObj(Vector2 center)
+        {
+           _instantiatedCenterObj = Instantiate(centerObjPrefab);
+            _instantiatedCenterObj.transform.position = center;
+            foreach (var neighbour in _lastSelectionList)
+            {
+                neighbour.SelfGameObject.transform.SetParent(_instantiatedCenterObj.transform);
+            }
+
+        }
+
+       
+    }
+}
+
---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HexagonDemo.InputData
+{
+
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] InputData _inputData;
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && MapState.GameStateInfo == GameState.Filled)
+            {
+                _inputData.Click();
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                _inputData.RotateHexagons();
+                MapState.GameStateInfo = GameState.Rotating;
+                
+            }
+        }
+
+    }
+}
