07e21b061336066395654a29e727d33408fa3c74
UfukSenkal <67637340+UfukSenkal@users.noreply.github.com>
2021-10-14 18:39:25 +0300

    +add score system

---
+++ Assets/Scripts/Score/ScoreController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace HexagonDemo.Score
+{
+    public class ScoreController : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI _scoreText;
+        [SerializeField] int _scoreMult = 5;
+        [SerializeField] string scoreString = "Score: ";
+        int _score = 0;
+
+        public int Score { get => _score;}
+
+
+
+        public void ScoreTextUpdate(int hexagonCount)
+        {
+            _score += (hexagonCount * _scoreMult);
+            _scoreText.text = scoreString + Score;
+        }
+    }
+}
